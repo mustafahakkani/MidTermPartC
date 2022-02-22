da5ad4baa90326804d2bfde90a7414c0b2aeb387
mustafahakkani <hakkani@sheridancollege.ca>
2022-02-22 18:22:27 +0300

    MidTermPartC.

---
+++ src/TestUserprofile/TestUserprofile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestUserprofile;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author musta
+ */
+public class TestUserprofile {
+    
+    private String name;
+    Scanner scn =new Scanner(System.in);
+    public static void main(String[]args){
+    
+    System.out.print("Enter your name:");
+    name=scn.nextLine();
+   
+}
+}
