9950cc3fa346fb59a2b28ab8cd03534ff8421ab4
mustafahakkani <hakkani@sheridancollege.ca>
2022-02-22 18:45:31 +0300

    Branch

---
+++ src/Mustafa/Mustafa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mustafa;
+
+/**
+ *
+ * @author musta
+ */
+public class Mustafa {
+    
+    /*my name is mustafa hakkani and this is my mid term
+    
+    */
+    
+}
--- src/TestUserprofile/TestUserprofile.java
+++ src/TestUserprofile/TestUserprofile.java
@@ -12,13 +12,25 @@ import java.util.Scanner;
  * @author musta
  */
 public class TestUserprofile {
+    public static void main(String[] args)
+    {
+       String[] genres = {"Drama", "Action", "Comedy", "Mystery"};
+       System.out.println("genres: ");
+        for (String genre : genres) {
+            System.out.println("    " + genre);
+        }
+        Scanner scn1=new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name=scn1.nextLine();
+       
+        Scanner scn2=new Scanner(System.in);
+        System.out.print("Genre: ");
+        String gen;
+        gen = scn1.nextLine();
+     
+        System.out.println("Profile is created!");
+        
+        
+    }
     
-    private String name;
-    Scanner scn =new Scanner(System.in);
-    public static void main(String[]args){
-    
-    System.out.print("Enter your name:");
-    name=scn.nextLine();
-   
-}
 }
